94b256c1e86a9d582644d7f32ce969d28cc32fa0
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2024-11-23 22:47:50 -0300

    sono

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './styles/Navigation.css';
 import Contatos from './Navigation_component/Contato';
+import Stacks from './Navigation_component/Stacks';
 
 const Navigation = () => {
   return (
     <section className='section_stacks_navigation'>
+      <Stacks />
       <div className='div_navigation'>1</div>
       <div className='div_navigation'>3</div>
-      <div className='div_navigation'>4</div>
       <Contatos />
     </section>
   );
